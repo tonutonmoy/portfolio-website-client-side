b27813bd8efe06c6cba1ab332e97d675f9408ddf
tonutonmoy <www.tonutonmoy12@gmail.com>
2023-06-30 19:52:46 +0600

    footer icons done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import { Outlet } from 'react-router-dom'
 import Navbar from './Component/Navbar/Navbar'
+import Footer from './Component/Footer/Footer'
 
 function App() {
  
@@ -10,6 +11,7 @@ function App() {
 <div>
   <Navbar></Navbar>
   <Outlet></Outlet>
+  <Footer></Footer>
 </div>
   )
 }
---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import { BsFacebook,BsLinkedin } from 'react-icons/bs';
+import {GrInstagram } from 'react-icons/gr';
+
+
+const Footer = () => {
+    return (
+<footer className="footer footer-center p-4  text-white/60 mt-40 py-[50px] 
+">
+
+
+<div className=' flex gap-3 pb-10 border-b-[1px] border-white/60
+w-[50%] mx-auto  justify-center'>
+  <a href="https://www.facebook.com/tonu.tonmoy.3">
+  <BsFacebook className='text-[30px]'/>
+  </a>
+
+  <a href="https://www.instagram.com/show_mik_arefin_tonmoy/">
+  <GrInstagram className='text-[30px]'/>
+  </a>
+
+  <a href="">
+  <BsLinkedin className='text-[30px]'/>
+  </a>
+</div>
+
+  <section>
+    <p className="]   font-[400]  ">Copyright © 2023 <span className="text-blue-500">
+    Showmik Arifen Tonmoy</span>. All right reserved  </p>
+  </section>
+</footer>
+    );
+};
+
+export default Footer;
