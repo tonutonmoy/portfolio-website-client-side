ce54eb681badf9fbd9415406672e5c70fb37a351
tonutonmoy <www.tonutonmoy12@gmail.com>
2023-10-25 18:40:22 +0600

    changed Resume

--- src/Component/Resume/Resume.jsx
+++ src/Component/Resume/Resume.jsx
@@ -5,8 +5,8 @@ const Resume = () => {
 
     title.innerText='Resume' 
     return (
-        <div className=" pt-[150px] px-[10px]  md:px-[30px] lg:px-[30px]  xl:px-[30px]  2xl:px-[30px]  w-[90%] md:w-[90%] lg:w-[70%] xl:w-[50%] 2xl:w-[40%] mx-auto flex justify-center ">
-            <iframe className=" w-full h-[1000px]"  src="https://docs.google.com/document/d/e/2PACX-1vQ0PXg3KS7Xjz-PUY7BSoMlZXmgo0RRKlkgt1AX-mTDDZVLaEJI06v85Q4dWLRiUkaNCD4lZx9FpnBs/pub?embedded=true"  allow="autoplay" ></iframe>
+        <div className=" pt-[150px] px-[10px]  md:px-[30px] lg:px-[30px]  xl:px-[30px]  2xl:px-[30px]  w-[90%] md:w-[90%] lg:w-[70%] xl:w-[50%] 2xl:w-[50%] mx-auto flex justify-center ">
+            <iframe className=" w-full h-[1000px]  "  src="https://docs.google.com/document/d/e/2PACX-1vSHg2toJkCFP0dDp6X_7mJsrPAcDm5bwdvBFFtCc2p24W7cXI1jb0vfEIc8H5kATM8fRcUWPY4aCYxZ/pub?embedded=true"  allow="autoplay" ></iframe>
         </div>
     );
 };
